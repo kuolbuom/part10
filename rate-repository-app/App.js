b120bfa82e1c73021fdd800f4547bd6d7dd29fa3
kuol buom <kuolbuom111@gmail.com>
2026-09-01 12:57:33 +0300

    Exercise: 1 initialize the application, Fix rate repository app structure

---
+++ rate-repository-app/App.js
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+  console.log("Hello World, this is my first React Native app!");
+  return (
+    <View>
+      <Text>Welcome to React Native!, from very beginning</Text>
+    </View>
+  );
+}
