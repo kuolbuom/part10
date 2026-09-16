ad87ddc9a5b1de573d727af0424e435d32b5406d
kuol buom <kuolbuom111@gmail.com>
2026-09-16 12:25:18 +0300

    Exercise: 23. Sorting the reviewed repositories list

--- rate-repository-app/src/components/RepositoryListContainer.js
+++ rate-repository-app/src/components/RepositoryListContainer.js
@@ -1,12 +1,24 @@
 import { FlatList, View, StyleSheet, Pressable } from "react-native";
+import { useState } from "react";
+import { Picker } from "@react-native-picker/picker";
 
 import { useNavigate } from "react-router-native";
+import theme from "./theme";
 
 import RepositoryItem from "./RepositoryItem";
 import useRepositories from "../hooks/useRepositories";
 import Text from "./Text";
 
 const styles = StyleSheet.create({
+  picker: {
+    backgroundColor: theme.colors.mainBackground,
+    border: "none",
+    padding: 30,
+    height: 144,
+    width: "100%",
+    color: "#000000",
+    fontSize: 30,
+  },
   separator: {
     height: 10,
   },
@@ -14,8 +26,17 @@ const styles = StyleSheet.create({
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
+const sortingOptions = {
+  latest: { orderBy: "CREATED_AT", orderDirection: "DESC" },
+  highest: { orderBy: "RATING_AVERAGE", orderDirection: "DESC" },
+  lowest: { orderBy: "RATING_AVERAGE", orderDirection: "ASC" },
+};
+
 const RepositoryListContainer = () => {
-  const { repositoriesNodes, loading, error } = useRepositories();
+  const [sort, setSort] = useState("latest");
+  const { repositoriesNodes, loading, error } = useRepositories(
+    sortingOptions[sort],
+  );
 
   const navigate = useNavigate();
 
@@ -32,6 +53,20 @@ const RepositoryListContainer = () => {
     <FlatList
       data={repositoriesNodes}
       ItemSeparatorComponent={ItemSeparator}
+      ListHeaderComponent={
+        <View>
+          <Picker
+            style={styles.picker}
+            prompt="Select an item..."
+            selectedValue={sort}
+            onValueChange={setSort}
+          >
+            <Picker.Item label="Latest repositories" value="latest" />
+            <Picker.Item label="Highest rated repositories" value="highest" />
+            <Picker.Item label="Lowest rated repositories" value="lowest" />
+          </Picker>
+        </View>
+      }
       renderItem={({ item }) => (
         <Pressable onPress={() => navigate(`/repository/${item.id}`)}>
           <RepositoryItem item={item} />
